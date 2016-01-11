38c455a512bb32713c5d7eb119f3b5049b48db6f
mdpgfu <mdprice1@gmail.com>
2016-01-11 13:13:59 -0800

    Add Room Class, Update Main Loop
    Added Rooms, First iteration of main loop

--- ok.py
+++ ok.py
@@ -2,33 +2,48 @@ class_list = ('1: Warrior', '2: Ninja', '3: Priest', '4: Wizard')
 race_list = ('1: Human', '2: Elf', '3: Goblin', '4: Orc')
 main_menu_list = ('1: Play', '2: Quit')
 
+
 def print_menu(menu_name):
     for i in menu_name:
         print i
-        
+
+
 def get_menu_choice():
     x = raw_input("Choose an option: ")
     return x
 
+
 def get_player_name():
     player_name = raw_input("Choose your character's name: ")
     return player_name
+
 
 def get_player_race():
     print_menu(race_list)
     menu_choice = get_menu_choice()
     return menu_choice
 
+
 def get_player_class():
     print_menu(class_list)
     menu_choice = get_menu_choice()
     return menu_choice
+
+
+class Room:
+    def __init__(self):
+        self.room_id = 1
+        self.room_name = "Town Square"
+        self.room_description = "This is a bustling town square."
+        self.room_inhabitants = "Mayor Greenling"
+        self.room_exits = {'North', 'East', 'South', 'West'}
 
 class Character:
     def __init__(self):
         self.name = ""
         self.current_health = 1
         self.health_max = 1
+
 
 class Player(Character):
     def __init__(self, name, race, player_class):
@@ -39,36 +54,69 @@ class Player(Character):
         self.race = race
         self.player_class = player_class
 
-"""For Debug"""
-def print_stats(character):
-    print "Name", character.name
-    print "Race", character.race
-    print "Class", character.player_class
-    print "Health", character.current_health, '/', character.max_health
+        self.player_room = 1
 
-##MAIN GAME SECTION
-print_menu(main_menu_list)
-main_menu_choice = get_menu_choice()
-if main_menu_choice == '1':
+    def attack(self,target):
+        print "%s attacks %s for 1 damage!", self.name
+
+    def get_inventory(self):
+        print 'This is %s inventory', self.name
+
+    def print_stats(self):
+        print "Name", self.name
+        print "Race", self.race
+        print "Class", self.player_class
+        print "Health", self.current_health, '/', self.health_max
+
+
+def initialize_player():
     x = get_player_name()
     y = get_player_race()
     z = get_player_class()
     new_player = Player(x, y, z)
-    print_stats(new_player)
+    new_player.print_stats()
+    return new_player
+
+
+def get_action():
+    action = raw_input("Enter Action:")
+    return action
+
+
+def player_action(action, player_id):
+    if action == "health":
+        print "Health = ", player_id.current_health
+
+
+def display_room(room_number):
+    print room_number.room_name
+    print room_number.room_description
+    print "Also here:", room_number.room_inhabitants
+    print "Exits:", room_number.room_exits
+
+
+def game_loop():
+    player_1 = initialize_player()
+    room1 = Room()
+    x = True
+    while x == 1:
+        display_room(room1)
+        next_act = get_action()
+        player_action(next_act, player_1)
+
+
+
+
+
+
+# MAIN GAME SECTION
+print_menu(main_menu_list)
+main_menu_choice = get_menu_choice()
+if main_menu_choice == '1':
+    game_loop()
 else:
     print "Peace Out"
 
-
-##def generate_character():
-##    new_character = Player()
-##
-##def launch_game():
-##    generate_character()
-##    
-##def run_game_loop():
-##    state = True
-##    while state == True:
-##        print 'OK'
 
 
 
