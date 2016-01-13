6332dfb62cbc7d68b029b14c8ae148a7c5cba97f
mdpgfu <mdprice1@gmail.com>
2016-01-13 08:55:46 -0800

    Fixed comments

--- ok.py
+++ ok.py
@@ -2,27 +2,22 @@ class_list = ('1: Warrior', '2: Ninja', '3: Priest', '4: Wizard')
 race_list = ('1: Human', '2: Elf', '3: Goblin', '4: Orc')
 main_menu_list = ('1: Play', '2: Quit')
 
-
 def print_menu(menu_name):
     for i in menu_name:
         print i
-
-
+        
 def get_menu_choice():
     x = raw_input("Choose an option: ")
     return x
-
 
 def get_player_name():
     player_name = raw_input("Choose your character's name: ")
     return player_name
 
-
 def get_player_race():
     print_menu(race_list)
     menu_choice = get_menu_choice()
     return menu_choice
-
 
 def get_player_class():
     print_menu(class_list)
@@ -38,13 +33,11 @@ class Room:
         self.room_inhabitants = "Mayor Greenling"
         self.room_exits = {'North', 'East', 'South', 'West'}
 
-
 class Character:
     def __init__(self):
         self.name = ""
         self.current_health = 1
         self.health_max = 1
-
 
 class Player(Character):
     def __init__(self, name, race, player_class):
@@ -55,28 +48,22 @@ class Player(Character):
         self.race = race
         self.player_class = player_class
 
-        self.player_room = 1
 
-    def attack(self,target):
-        print "%s attacks %s for 1 damage!", self.name
+def print_stats(character):
+    print "Name", character.name
+    print "Race", character.race
+    print "Class", character.player_class
+    print "Health", character.current_health, '/', character.max_health
 
-    def get_inventory(self):
-        print 'This is %s inventory', self.name
-
-    def print_stats(self):
-        print "Name", self.name
-        print "Race", self.race
-        print "Class", self.player_class
-        print "Health", self.current_health, '/', self.health_max
-
-
-def initialize_player():
+# MAIN GAME SECTION
+print_menu(main_menu_list)
+main_menu_choice = get_menu_choice()
+if main_menu_choice == '1':
     x = get_player_name()
     y = get_player_race()
     z = get_player_class()
     new_player = Player(x, y, z)
     new_player.print_stats()
-    return new_player
 
 
 def get_action():
@@ -105,19 +92,13 @@ def game_loop():
         next_act = get_action()
         player_action(next_act, player_1)
 
-
-
-
-
-# test
 # MAIN GAME SECTION
 print_menu(main_menu_list)
 main_menu_choice = get_menu_choice()
 if main_menu_choice == '1':
     game_loop()
+    print_stats(new_player)
 else:
     print "Peace Out"
-
-
 
 
